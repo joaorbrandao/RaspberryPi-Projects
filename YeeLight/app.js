ca67d65fc1733fa76bfafb1f4096cec1775cdd12
JoaoBrandao <joaorbrandao.dev@outlook.pt>
2017-07-13 23:43:42 +0100

    Add SSDP package to communicate with the bulb.

--- YeeLight/app.js
+++ YeeLight/app.js
@@ -1,14 +1,33 @@
 const http = require('http');
 var express = require('express');
 var app = express();
+var ssdp = require('node-ssdp').Client
+			, client = new ssdp({
+				unicastHost: '192.168.1.2',
+				unicastBindPort: 1982
+			});
 
 
 const hostname = '127.0.0.1';
 const port = 3000;
+DEBUG = 'node-ssdp*';
 
 
 app.get('/', function(request, response){
-	response.send('Hello World');
+	console.log('Creating request...');
+
+	client.on('response', function inResponse(headers, code, rinfo) {
+		console.log('Got a response to an m-search:\n%d\n%s\n%s', code, JSON.stringify(headers, null, '  '), JSON.stringify(rinfo, null, '  '))
+	});
+
+	client.on('notify', function () {
+		console.log('Got a notification.');
+	});
+
+	//Search for the bulb
+	client.search('urn:schemas-upnp-org:service:ContentDirectory:1');
+
+	client.send();
 });
 
 app.listen(port, hostname, function(){
